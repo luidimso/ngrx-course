a9e7e87acb3dc36d1ee05e1d5a71b5ed90fb1ec1
luidimso <luidi_mso@outlook.com>
2022-01-29 19:45:47 -0300

    Adding meta reducer function

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -16,5 +16,14 @@ export const reducers: ActionReducerMap<State> = {
   router: routerReducer
 };
 
+export function logger(reducer:ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log("state before: ", state);
+    console.log("action: ", action);
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+    return reducer(state, action);
+  }
+}
+
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [logger] : [];
